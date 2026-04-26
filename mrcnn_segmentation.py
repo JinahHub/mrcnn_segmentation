2e4ea4279aeb6e2389e6b0569e18add1ca7d7171
peytonhong <hhs8314@gmail.com>
2026-04-26 21:39:28 +0900

    'debug'

--- mrcnn_segmentation.py
+++ mrcnn_segmentation.py
@@ -31,14 +31,7 @@ def get_transform(train):
     transforms.append(T.Resize((256,256)))
     return T.Compose(transforms)
 
-def main(args):
-    # transform = transforms.Compose([
-    #     transforms.ToTensor(),
-    #     transforms.Resize((256, 256)), # 크기 고정 확인
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    #     # transforms.ToPureTensor()
-    # ])
-    
+def main(args):    
 
     n_classes = 2
 
